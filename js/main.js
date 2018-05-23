0ae7ff9abd5f8d80b4fad1fdf71672b73a8028a7
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-05-22 20:29:32 -0400

    working drag and drop

--- js/main.js
+++ js/main.js
@@ -5,14 +5,44 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
 
+// Loop through empties and call drag events
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
 // Drag Functions
 function dragStart() {
-  console.log('grabbed');
+  console.log('start');
   this.className += ' hold';
   setTimeout(() => (this.className = 'invisible'), 0);
 }
 
 function dragEnd() {
-  console.log('released');
+  console.log('end');
   this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  console.log('over');
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+  console.log('enter');
+}
+
+function dragLeave() {
+  this.className = 'empty';
+  console.log('leave');
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+  console.log('drop');
 }
